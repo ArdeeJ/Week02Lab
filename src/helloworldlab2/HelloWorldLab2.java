b8ccad2bb6773bfa00490676344e606ccbc4006a
ardee <ardee@Ardee-De-Ocera>
2022-09-08 14:35:36 -0600

    Wassup Lab 2

---
+++ src/helloworldlab2/HelloWorldLab2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldlab2;
+
+/**
+ *
+ * @author ardee
+ */
+public class HelloWorldLab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Follow me on IG @ardeethemighty");
+        int a = 1;
+        int b = 5;
+        int c = a + b;
+        System.out.println(c);
+    }
+    
+}
